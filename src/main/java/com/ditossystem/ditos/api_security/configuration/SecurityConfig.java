866f63905672956d0a0c9a52de3d0a8989a378d6
Matheus Silva <scruz.math@gmail.com>
2025-03-12 16:37:21 -0300

    Mudança do arquivo de CORS para SecurityConfig

--- src/main/java/com/ditossystem/ditos/api_security/configuration/SecurityConfig.java
+++ src/main/java/com/ditossystem/ditos/api_security/configuration/SecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -36,7 +41,20 @@ public class SecurityConfig {
 //                .addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 
                 )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*")); // Ou origens específicas
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
