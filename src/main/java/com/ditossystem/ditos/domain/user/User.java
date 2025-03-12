907d33900ed867dc7aa51a921e2100bc2663ade2
Matheus Silva <scruz.math@gmail.com>
2025-03-12 15:03:05 -0300

    Criação da Entidade User

---
+++ src/main/java/com/ditossystem/ditos/domain/user/User.java
@@ -0,0 +1,60 @@
+package com.ditossystem.ditos.domain.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Document(collection = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+public class User implements UserDetails {
+
+    @Id
+    @JsonProperty("id")
+    private String id;
+
+    private String login;
+    private String password;
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new  SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
---
+++ src/main/java/com/ditossystem/ditos/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ditossystem.ditos.repository;
+
+import com.ditossystem.ditos.domain.user.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    UserDetails findByLogin(String login);
+}
