39bcd05d2c3b77bcdd51ffb230b09a3da55f7687
Matheus Silva <scruz.math@gmail.com>
2025-03-14 16:48:16 -0300

    Criação do endpoint users

---
+++ src/main/java/com/ditossystem/ditos/controller/UserController.java
@@ -0,0 +1,76 @@
+package com.ditossystem.ditos.controller;
+
+import com.ditossystem.ditos.domain.user.UserDTO;
+import com.ditossystem.ditos.infra.security.SecurityUtils;
+import com.ditossystem.ditos.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public  UserController(UserService userService, SecurityUtils securityUtils) {
+        this.userService = userService;
+    }
+
+    // MÉTODOS GETS
+    // Método Get All
+    // Endpoint para listar todos os users(GET)
+    @GetMapping
+    public ResponseEntity<?> getAllUsers(){
+
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    // Endpoint para buscar user pelo id (GET)
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable String id){
+        Optional<UserDTO> user = userService.getUserById(id);
+
+        return user
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    // Endpoint para buscar users que possuem o mesmo login (GET)
+    // /users/search?name=<name>
+    @GetMapping("/search")
+    public ResponseEntity<List<UserDTO>> getUsersByName(@RequestParam String name){
+
+        List<UserDTO> users = userService.getUsersByLogin(name);
+
+        return users.isEmpty()
+                ? ResponseEntity.notFound().build()
+                : ResponseEntity.ok(users);
+    }
+
+    // Método PUT
+    // Endpoiint para editar um User (PUT)
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable String id, @RequestBody UserDTO userDTO){
+        return userService.updateUser(id, userDTO)
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    // Método Delete
+    // Endpoint para deletar um user (DELETE)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable String id){
+        boolean result =  userService.deleteUser(id);
+        if(result){
+            return ResponseEntity.status(HttpStatus.OK).body("User deletado com sucesso!");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User não encontrado!");
+    }
+}
