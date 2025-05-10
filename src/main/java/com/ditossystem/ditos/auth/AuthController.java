c8e4d3dd2bab99ca5a60acea22089ba6e4f6c2bd
Matheus Silva <scruz.math@gmail.com>
2025-05-10 15:35:56 -0300

    Correção de login

--- src/main/java/com/ditossystem/ditos/auth/AuthController.java
+++ src/main/java/com/ditossystem/ditos/auth/AuthController.java
@@ -2,19 +2,19 @@ package com.ditossystem.ditos.auth;
 
 import com.ditossystem.ditos.exception.UserAlreadyExistsException;
 import com.ditossystem.ditos.security.TokenService;
-import com.ditossystem.ditos.user.UserRepository;
 import com.ditossystem.ditos.auth.dto.AuthDTO;
 import com.ditossystem.ditos.auth.dto.LoginResponseDTO;
 import com.ditossystem.ditos.user.dto.RegisterDTO;
 import com.ditossystem.ditos.user.UserService;
 import com.ditossystem.ditos.user.dto.UserDTO;
 import com.ditossystem.ditos.user.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,12 +28,13 @@ public class AuthController {
     private final TokenService tokenService;
     private final UserService userService;
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     @Autowired
     public AuthController(
             AuthenticationManager authenticationManager,
-            UserRepository userRepository,
-            PasswordEncoder passwordEncoder,
-            TokenService tokenService, UserService userService) {
+            TokenService tokenService,
+            UserService userService) {
         this.authenticationManager = authenticationManager;
         this.tokenService = tokenService;
         this.userService = userService;
@@ -41,12 +42,23 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthDTO data){
+        logger.info("Tentando logar com login: {}", data.login());
+
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
 
-        var token = tokenService.generateToken((User) auth.getPrincipal());
+        try{
+            var auth = this.authenticationManager.authenticate(usernamePassword);
+            logger.info("Usuário autenticado: {}", auth.getName());
 
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+            var token = tokenService.generateToken((User) auth.getPrincipal());
+            logger.info("Token gerado: {}", token);
+
+            return ResponseEntity.ok(new LoginResponseDTO(token));
+        }
+        catch (Exception e){
+            logger.error("Erro ao autenticar usuário", e);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+        }
     }
 
     @PostMapping("/register")
--- src/main/java/com/ditossystem/ditos/security/SecurityFilter.java
+++ src/main/java/com/ditossystem/ditos/security/SecurityFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -27,14 +28,22 @@ public class SecurityFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
+        
         if(token != null){
             var login = tokenService.validateToken(token);
-            UserDetails user = userRepository.findByLogin(login);
+            
+            if(login != null && !login.isBlank()){
+                UserDetails user = userRepository.findByLogin(login);
 
-            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+                if(user != null){
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
         }
         filterChain.doFilter(request, response);
     }
